3051677f0f1fbcfd46b4ffcd5ddfb4c2e12042af
Max <example@gmail.com>
2020-09-11 15:46:24 +0500

    Massive and type.of

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 "use strict";
 
-let num = 50;
+// alert('Hello');
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+// const result = confirm("Are you here?");
+// console.log(result);
 
-// do {
-//     console.log(num);
-//     num++;
-// } 
-// while (num < 55);
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
+const answers = [];
 
-    console.log(i);
-}
+// answers[0] = prompt("Как Ваше имя?", "");
+// answers[1] = prompt("Как Ваше фамилия?", "");
+// answers[2] = prompt("Сколько Вам лет?", "");
+
+console.log(typeof(answers));
+
+// console.log(typeof(null));
+
+// documet.write(answers);
