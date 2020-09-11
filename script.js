76055acf093912170ff11dd03a348b273c2f491c
Max <example@gmail.com>
2020-09-11 15:48:19 +0500

    Interpolation

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 "use strict";
 
-// alert('Hello');
+const category = 'toys';
 
-// const result = confirm("Are you here?");
-// console.log(result);
+console.log(`https://someurl.com/${category}/5`);
 
-// const answer = +prompt("Вам есть 18?", "18");
-// console.log(answer + 5);
+const user = 'Ivan';
 
-const answers = [];
-
-// answers[0] = prompt("Как Ваше имя?", "");
-// answers[1] = prompt("Как Ваше фамилия?", "");
-// answers[2] = prompt("Сколько Вам лет?", "");
-
-console.log(typeof(answers));
-
-// console.log(typeof(null));
-
-// documet.write(answers);
+alert(`Привет, ${user}`);
