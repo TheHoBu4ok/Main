afd0b36c7de8ea36401252412cf1fb4d5d096bf3
Max <example@gmail.com>
2020-09-22 15:51:20 +0500

    Objects and destructuring objects

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 "use strict";
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
-second();
+const {border, bg} = options.colors;   // Деструктуризация
+console.log(border);
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+// console.log(Object.keys(options).length);
 
-// learnJS('JavaScript', function() {
-//     console.log('Я прошел этот урок!');
-// });
+// console.log(options["colors"]["border"]);
 
-function done() {
-    console.log('Я прошел этот урок!');
-}
+// delete options.name;
 
-learnJS('JavaScript', done);
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
