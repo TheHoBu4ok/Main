8bfc5887880d2b44231eb8723a7824132df3d81f
Max <example@gmail.com>
2020-11-05 12:51:39 +0500

    Dom-elements navigation, data-atributes, advantage for/of

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
 "use strict";
 
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
 
-// btn.onclick = function() {
-//     alert('Click');
-// };
+// console.log(document.querySelector('#current').parentNode.parentNode);
 
-// btn.addEventListener('click', () => {
-//     alert('Second click');
-// });
+// console.log(document.querySelector('#current').parentNode.parentElement);
 
-// let i = 0;
-const deleteElement = (e) => {
-    console.log(e.currentTarget);
-    console.log(e.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
-};
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    console.log(event.target);
-});
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    
+    console.log(node);
+}
