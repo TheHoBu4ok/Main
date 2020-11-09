96bd40577ddc14d980e7009e6b1cef7fa3973837
Max <example@gmail.com>
2020-11-09 16:10:51 +0500

    Enents on mobile phones

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 "use strict";
 
-// console.log(document.head);
-// console.log(document.documentElement);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.firstElementChild);
-// console.log(document.body.lastChild);
+// touchstart
+// touchmove
+// touchend
+// touchenter - заход в активную зону
+// touchleave - выход с активной зоны
+// touchcancel - за пределы
 
-// console.log(document.querySelector('#current').parentNode.parentNode);
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
 
-// console.log(document.querySelector('#current').parentNode.parentElement);
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
 
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+        console.log('Start');
+        console.log(e.targetTouches);
+    });
 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    
-    console.log(node);
-}
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log(e.targetTouches[0].pageX);
+    });
+
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('End');
+    // });
+});
+
+// touches
+// targetTouches
+// changedTouches
